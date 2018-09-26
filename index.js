f65854292f28e6eb89cbfee317bebd4d4275df18
Seneca Manu <dody@Seneca-Manu.local>
2018-09-26 23:51:28 +0700

    Release 2

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 65534;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 const model = require('./models/index.js');
 const Teacher = model.Teacher;
 const Subject = model.Subject;
 
 app.get('/', function(req, res) {
-    res.send(`hello!`);
+    res.render('index.ejs', {test: 'hi'});
 })
 
-//TEACHER DISPLAY
+//TEACHER PAGE
 app.get('/teacher', function(req, res) {
     Teacher.findAll({
         include: [{
@@ -18,20 +24,52 @@ app.get('/teacher', function(req, res) {
         }]
     })
         .then(function(teacher) {
-            res.send(teacher)
+            // res.send(teacher)
+            res.render('teacher.ejs', {teacher: teacher});
         })
         .catch(function(err) {
             res.send(err)
         })
 })
 
-//SUBJECT DISPLAY
+// TEACHER DISPLAY
+app.get('/teacherdisplay', function (req, res) {
+    Teacher.findAll({
+            include: [{
+                model: Subject
+            }]
+        })
+        .then(function (teacher) {
+            // res.send(teacher)
+            res.send(teacher);
+        })
+        .catch(function (err) {
+            res.send(err)
+        })
+})
+
+//SUBJECT PAGE
 app.get('/subject', function (req, res) {
     Subject.findAll({
         include: [{
             model: Teacher
         }]
     })
+        .then(function (subject) {
+            res.render('subject.ejs', {subject: subject})
+        })
+        .catch(function (err) {
+            res.send(err)
+        })
+})
+
+//SUBJECT DISPLAY
+app.get('/subjectdisplay', function (req, res) {
+    Subject.findAll({
+            include: [{
+                model: Teacher
+            }]
+        })
         .then(function (subject) {
             res.send(subject)
         })
